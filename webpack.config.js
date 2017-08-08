0a3094f7f6f7f65dd91fb0a5c47f2b0cc8f46bce
iturgeon <iturgeon@gmail.com>
2017-08-07 23:28:11 -0400

    updates build after materia-client-assets doesnt auto build

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
 const path              = require('path')
 const CleanPlugin       = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MateriaClientAssetWebpack = require('materia-client-assets/webpack.config.js')
 
 const mdkSrcPath  = path.resolve(__dirname, 'src');
 const corePath    = path.join('materia-core', 'coffee', 'materia');
 const buildPath   = path.resolve('build') + path.sep
 
-module.exports = {
-	entry: {
-		'mdk-splash.js': [
-			path.join(mdkSrcPath, 'mdk.splash.coffee')
-		],
-		'mdk-player.js': [
-			path.join(mdkSrcPath, 'mdk.player.coffee'),
-		],
-		'mdk-package.js': [
-			path.join(mdkSrcPath, 'mdk.package.coffee'),
-		],
-	},
+module.exports = [
+	{
+		entry: {
+			'mdk-splash.js': [
+				path.join(mdkSrcPath, 'mdk.splash.coffee')
+			],
+			'mdk-player.js': [
+				path.join(mdkSrcPath, 'mdk.player.coffee'),
+			],
+			'mdk-package.js': [
+				path.join(mdkSrcPath, 'mdk.package.coffee'),
+			],
+		},
 
-	// write files to the outputPath (default = ./build) using the object keys from 'entry' above
-	output: {
-		path: buildPath,
-		filename: '[name]',
-		publicPath: buildPath
-	},
+		// write files to the outputPath (default = ./build) using the object keys from 'entry' above
+		output: {
+			path: buildPath,
+			filename: '[name]',
+			publicPath: buildPath
+		},
 
-	module: {
-		rules: [
-			{
-				test: /\.coffee$/i,
-				loader: ExtractTextPlugin.extract({
-					use: ['raw-loader', 'coffee-loader']
-				})
-			}
+		module: {
+			rules: [
+				{
+					test: /\.coffee$/i,
+					loader: ExtractTextPlugin.extract({
+						use: ['raw-loader', 'coffee-loader']
+					})
+				}
+			]
+		},
+		plugins: [
+			new CleanPlugin([buildPath]),
+			new ExtractTextPlugin({filename: '[name]'}),
 		]
 	},
-	plugins: [
-		new CleanPlugin([buildPath]),
-		new ExtractTextPlugin({filename: '[name]'}),
-	]
-}
+	MateriaClientAssetWebpack
+]
