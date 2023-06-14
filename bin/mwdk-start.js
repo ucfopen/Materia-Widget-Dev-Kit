9700f6e4796293524c1593524d8adcbb6f53b1e8
Cay Henning <cay@ucf.edu>
2023-06-14 11:49:52 -0400

    enables template MWDK startup

--- bin/mwdk-start.js
+++ bin/mwdk-start.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
-shell.exec("env RUNNING_DEV_SERVER=true node ./node_modules/materia-widget-development-kit/express.js --mode=development");
+shell.exec("node ./node_modules/materia-widget-development-kit/express.js --mode=development");
