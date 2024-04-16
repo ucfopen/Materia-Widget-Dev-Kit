a5778e45865518daaeb5e5f2ee3373fdd645be6f
Cay Henning <cay@ucf.edu>
2024-04-16 08:49:57 -0400

    Allow flexibility in node_env

--- bin/mwdk-start.js
+++ bin/mwdk-start.js
@@ -5,4 +5,11 @@ const args = process.argv.slice(2);
 
 const shell = require("shelljs");
 
-shell.exec(`export NODE_ENV=development || set NODE_ENV=development && node ./node_modules/materia-widget-development-kit/express.js ${args.join(' ')}`);
+// check if the NODE_ENV is set to anything
+// if so, don't change it
+let node_env = process.env.NODE_ENV;
+if (node_env === undefined || node_env === null || node_env === '') {
+    node_env = 'production';
+}
+
+shell.exec(`export NODE_ENV=${node_env} || set NODE_ENV=${node_env} && node ./node_modules/materia-widget-development-kit/express.js ${args.join(' ')}`);
