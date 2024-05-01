9a194d259d19cc505ffcfd44e18f9945c39b6f23
Cay Henning <cay@ucf.edu>
2024-05-01 12:00:29 -0400

    add resolve to reactLoader

--- webpack-widget.js
+++ webpack-widget.js
@@ -144,6 +144,9 @@ const getDefaultRules = () => ({
 	reactLoader: {
 		test: /\.(js|jsx)$/i,
 		exclude: /node_modules/,
+		resolve: {
+			extensions: ['.js', '.jsx']
+		},
 		use: [
 			{
 				loader: 'babel-loader',
